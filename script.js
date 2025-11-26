77dd33e70a704aff19b19e3bdc6b32371e72c66a
Alexandre Paschoal <alexandrepasch@gmail.com>
2025-11-26 15:31:34 -0300

    Criei botoes, historico de chamadas e mudança no layout

--- script.js
+++ script.js
@@ -1,74 +1,126 @@
 const vSenha = document.querySelector(".senha1")
-let senhaNormal = localStorage.getItem("senhaNormal")
+let senhaNormal = Number(localStorage.getItem("senhaNormal")) || 0
+
 const pSenha = document.querySelector(".senha2")
-let senhaPreferencial = localStorage.getItem("senhaPreferencial")
+let senhaPreferencial = Number(localStorage.getItem("senhaPreferencial")) || 0
+
 const eSenha = document.querySelector(".senha3")
-let senhaExame = localStorage.getItem("senhaExame")
-let ultSenha = localStorage.getItem("ultSenha")
+let senhaExame = Number(localStorage.getItem("senhaExame")) || 0
 
-// ALTERAR AUDIO AQUI!!//
-let audio1 = new Audio("normal.mp3")
-let audio2 = new Audio("prioridade.mp3")
-let audio3 = new Audio("exame.mp3")
+let ultSenha = localStorage.getItem("ultSenha") || ""
 
-function mostrarSenha() {
-  if (ultSenha == "N") {
-    vSenha.innerHTML =
-      "N" +
-      parseInt(senhaNormal).toLocaleString("pt-br", { minimumIntegerDigits: 3 })
-  } else {
-    pSenha.innerHTML =
-      "P" +
-      parseInt(senhaPreferencial).toLocaleString("pt-br", {
-        minimumIntegerDigits: 3,
-      })
-    eSenha.innerHTML =
-      "E" +
-      parseInt(senhaExame).toLocaleString("pt-br", { minimumIntegerDigits: 3 })
-  }
+const audioN = document.getElementById("audioNormal")
+const audioP = document.getElementById("audioPreferencial")
+const audioE = document.getElementById("audioExame")
+
+const listaHistorico = document.getElementById("listaHistorico")
+let historico = JSON.parse(localStorage.getItem("historico")) || []
+
+function atualizarTela() {
+  vSenha.innerHTML =
+    "N" + senhaNormal.toLocaleString("pt-br", { minimumIntegerDigits: 3 })
+
+  pSenha.innerHTML =
+    "P" + senhaPreferencial.toLocaleString("pt-br", { minimumIntegerDigits: 3 })
+
+  eSenha.innerHTML =
+    "E" + senhaExame.toLocaleString("pt-br", { minimumIntegerDigits: 3 })
 }
 
-if (!senhaNormal) senhaNormal = 0
+function piscarElemento(elemento) {
+  elemento.classList.add("piscar")
+  setTimeout(() => {
+    elemento.classList.remove("piscar")
+  }, 3000) // pisca por 3 segundos
+}
 
-if (!senhaPreferencial) senhaPreferencial = 0
+function adicionarHistorico(tipo, numero) {
+  const texto = `${tipo}${numero.toLocaleString("pt-br", {
+    minimumIntegerDigits: 3,
+  })}`
+  historico.unshift(texto)
+  if (historico.length > 15) historico.pop()
 
-if (!senhaExame) senhaExame = 0
+  localStorage.setItem("historico", JSON.stringify(historico))
+  renderHistorico()
+}
 
-if (!ultSenha) ultSenha = 0
+function renderHistorico() {
+  listaHistorico.innerHTML = ""
+  historico.forEach((item) => {
+    const li = document.createElement("li")
+    li.textContent = item
+    listaHistorico.appendChild(li)
+  })
+}
 
-mostrarSenha()
+function chamarSenha(tipo) {
+  let elemento
 
-window.addEventListener("keydown", function (e) {
-  switch (e.key) {
-    case "r":
-      ultSenha = "N"
-      senhaNormal = 0
-      senhaPreferencial = 0
-      senhaExame = 0
-      this.window.location.reload(true)
-      break
-    case "n":
-      senhaNormal++
-      ultSenha = "N"
-      audio1.play()
-      break
-    case "p":
-      senhaPreferencial++
-      ultSenha = "P"
-      audio2.play()
-      break
-    case "e":
-      senhaExame++
-      ultSenha = "E"
-      audio3.play()
-      break
-    default:
-      break
+  if (tipo === "N") {
+    senhaNormal++
+    ultSenha = "N"
+    audioN.play()
+    elemento = vSenha
+    adicionarHistorico("N", senhaNormal)
   }
+
+  if (tipo === "P") {
+    senhaPreferencial++
+    ultSenha = "P"
+    audioP.play()
+    elemento = pSenha
+    adicionarHistorico("P", senhaPreferencial)
+  }
+
+  if (tipo === "E") {
+    senhaExame++
+    ultSenha = "E"
+    audioE.play()
+    elemento = eSenha
+    adicionarHistorico("E", senhaExame)
+  }
+
+  piscarElemento(elemento)
 
   localStorage.setItem("senhaNormal", senhaNormal)
   localStorage.setItem("senhaPreferencial", senhaPreferencial)
   localStorage.setItem("senhaExame", senhaExame)
   localStorage.setItem("ultSenha", ultSenha)
-  mostrarSenha()
+
+  atualizarTela()
+}
+
+atualizarTela()
+renderHistorico()
+
+document
+  .getElementById("btnNormal")
+  .addEventListener("click", () => chamarSenha("N"))
+document
+  .getElementById("btnPreferencial")
+  .addEventListener("click", () => chamarSenha("P"))
+document
+  .getElementById("btnExame")
+  .addEventListener("click", () => chamarSenha("E"))
+
+window.addEventListener("keydown", function (e) {
+  switch (e.key) {
+    case "r":
+      localStorage.clear()
+      location.reload()
+      break
+
+    case "n":
+      chamarSenha("N")
+      break
+
+    case "p":
+      chamarSenha("P")
+      break
+
+    case "e":
+      chamarSenha("E")
+      break
+  }
 })
